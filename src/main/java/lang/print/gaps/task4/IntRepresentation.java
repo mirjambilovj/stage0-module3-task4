5db98976c1bedd213c914eb41391ab39f139354c
mirjambilovj <106926296+mirjambilovj@users.noreply.github.com>
2022-07-31 22:58:14 +0500

    changed

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = Character.forDigit(99, 10);
+        char h = Character.forDigit(104, 10);
+        char a = Character.forDigit(97, 10);
+        char r = Character.forDigit(114, 10);
+        System.out.println(c + h + a + r);
     }
 }
